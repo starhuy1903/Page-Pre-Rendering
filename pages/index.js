97c00c8e0b52e2e2ae0dc2bf35d6b957b4f53c5f
Huy Nguyen <huykst137@gmail.com>
2022-10-07 21:25:21 +0700

    Demo Page-Pre-Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,44 @@
-import styles from '../styles/Home.module.css'
+import path from "path";
+import fs from "fs/promises";
+import Link from "next/link";
 
-export default function Home() {
+function Home(props) {
+  const { products } = props;
   return (
-    <div className={styles.container}>
-      <h1>Hello Next World!</h1>
+    <div>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
     </div>
-  )
+  );
 }
+
+// execute on server-side
+export async function getStaticProps(context) {
+  console.log("(Re-)Generating...");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default Home;
---
+++ pages/user-profile.js
@@ -0,0 +1,16 @@
+function UserProfilePage(props) {
+  return <h1>{props.username}</h1>;
+}
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  console.log("run");
+  return {
+    props: {
+      username: "Huy",
+    },
+  };
+}
